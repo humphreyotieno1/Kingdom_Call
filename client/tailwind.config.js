c4ff5304fec40867355b958791e7560badf403eb
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-08-13 22:40:31 +0300

    Project setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: ['flowbite/plugin'],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
