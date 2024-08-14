7fb1b074fcf6832719b7d0b63893fa44e259c295
Nyakio-Eva <evalkariuki@gmail.com>
2024-08-14 12:59:56 +0300

    add navbar, create pages files

---
+++ client/src/Pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+const AboutUsPage = () => {
+    return(
+        <div></div>
+    )
+}
+
+
+export default AboutUsPage;
---
+++ client/src/Pages/Media.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const MediaPage = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default MediaPage;
