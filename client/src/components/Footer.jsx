37a0eaf3870593690a10725529a2f60d771df7f9
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-08-15 02:10:49 +0300

    Added media fiAdded media files to the home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faLinkedin, faTelegram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUp, faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
@@ -9,19 +9,19 @@ const Footer = () => {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="flex flex-col items-center">
           <h2 className="text-2xl font-bold">Kingdom Call</h2>
-          <p className="text-sm mt-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit Nunc maximus</p>
+          <p className="text-sm mt-2 text-center">Your kingdom come...Your will be done.</p>
           <div className="flex mt-4 space-x-4">
             <a href="#" className="text-white"><FontAwesomeIcon icon={faFacebook} className="text-2xl" /></a>
             <a href="#" className="text-white"><FontAwesomeIcon icon={faInstagram} className="text-2xl" /></a>
-            <a href="#" className="text-white"><FontAwesomeIcon icon={faLinkedin} className="text-2xl" /></a>
+            <a href="#" className="text-white"><FontAwesomeIcon icon={faTelegram} className="text-2xl" /></a>
             <a href="#" className="text-white"><FontAwesomeIcon icon={faYoutube} className="text-2xl" /></a>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <h3 className="text-xl font-bold">Contact Us</h3>
-          <p className="mt-2"><FontAwesomeIcon icon={faPhone} /> +91 98765 43210</p>
-          <p className="mt-1"><FontAwesomeIcon icon={faEnvelope} /> info@example.com</p>
-          <p className="mt-1"><FontAwesomeIcon icon={faMapMarkerAlt} /> Lorem ipsum dolor sit amet</p>
+          <p className="mt-2"><FontAwesomeIcon icon={faPhone} /> +254 740 882 365</p>
+          <p className="mt-1"><FontAwesomeIcon icon={faEnvelope} /> wearekingdomcall@gmail.com</p>
+          <p className="mt-1"><FontAwesomeIcon icon={faMapMarkerAlt} /> Reach Out to Us</p>
         </div>
         <div className="flex flex-col items-center">
           <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className="bg-white text-black rounded-full p-2">
