12caa18c7796d2cf6768c0cbebd89dec0bf0bc59
Nyakio-Eva <evalkariuki@gmail.com>
2024-08-20 18:17:06 +0300

    update media and resources nav links

--- client/src/Pages/Media.jsx
+++ client/src/Pages/Media.jsx
@@ -12,9 +12,9 @@ const MediaPage = () => {
       </div>
 
       {/* Videos Section */}
-      <div className="videos-section py-10 bg-white">
+      <div className="videos-section py-10">
         <div className="container mx-auto px-4">
-          <h2 className="text-2xl md:text-3xl font-bold text-center mb-6">Videos</h2>
+          
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {/* Embedding the same video in each iframe */}
             <iframe 
--- client/src/Pages/Resources.jsx
+++ client/src/Pages/Resources.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const ResourcesPage = () => {
     return(
-        <div></div>
+        <div className="flex flex-col items-center justify-center py-12 bg-[#ebebeb]">
+            <div className="w-full py-4 bg-[linear-gradient(180deg,#e48515,#ebebeb)] py-10">
+                <h1 className="text-3xl font-bold text-black mt-20 mb-2 text-center py-4">Resources</h1>
+            </div>
+        </div>
     )
 }
 
