cf40ecef478189d6faa977c3a867fe77d7f2f4fe
Nyakio-Eva <evalkariuki@gmail.com>
2024-08-21 18:04:04 +0300

    update styling

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactUsPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center bg-[#ebebeb]">
+    <div className="flex flex-col items-center justify-center py-12 bg-[#ebebeb]">
       <div className="w-full py-4 bg-[linear-gradient(180deg,#e48515,#ebebeb)] py-10">
         <h1 className="text-3xl font-bold text-black mt-20 mb-2 text-center py-4">Get Help</h1>
       </div>
